fadb0b4814ab144eb0d38fbca7ec919207764215
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-06-26 17:42:10 +0530

    admin otp page

--- pages/reta/otpverification.js
+++ pages/reta/otpverification.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 
 const otpverification = () => {
-  return (
-    <div>otpverification</div>
-  )
-}
+  const [otp, setOtp] = useState("");
+  const inputRefs = useRef([]);
+  const maxLength = 1; // Maximum length for each input field
 
-export default otpverification
+  const handleKeyUp = (index, event) => {
+    if (event.target.value.length >= maxLength) {
+      // Check if the current input value has reached the maximum length
+      const nextIndex = index + 1;
+
+      if (nextIndex < inputRefs.current.length) {
+        // Check if there is a next input field
+        inputRefs.current[nextIndex].focus(); // Move the focus to the next input field
+      } else {
+        // If it's the last input field, gather all input values to form the OTP
+        const otpValues = inputRefs.current.map((ref) => ref.value);
+        const otpCode = otpValues.join("");
+        setOtp(otpCode);
+      }
+    }
+  };
+  console.log(otp);
+  return (
+    <div className="bg-[#1E1E1E] h-screen align-middle flex justify-center">
+      <div className="  text-white  text-center my-auto space-y-5">
+        <div className="flex justify-center">
+          <Image
+            width="100"
+            height="100"
+            src="/pagesgraphics/admin/login/otpverification.svg"
+          />
+        </div>
+
+        <h1 className="text-2xl">Admin Verification</h1>
+        <p className=" text-gray-600">
+          Enter OTP Code sent to <span>+91 1234567890</span>{" "}
+        </p>
+        <div className="flex gap-5 justify-center pt-10 pb-5 ">
+          {Array.from({ length:5 }, (_, index) => (
+            <input
+              key={index}
+              type="text"
+              maxLength={maxLength}
+              className="w-[75px] h-[75px] bg-[#373A41] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+              ref={(el) => (inputRefs.current[index] = el)} // Save the reference to each input field
+              onKeyUp={(event) => handleKeyUp(index, event)} // Call the event handler for each input field
+            />
+          ))}
+        </div>
+        {/* <div className="flex gap-5 justify-center">
+          <input
+            type="text"
+            maxLength="1"
+            className="w-[75px] h-[75px] bg-[#797676] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+          />
+          <input
+            type="text"
+            maxLength="1"
+            className="w-[75px] h-[75px] bg-[#797676] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+          />
+          <input
+            type="text"
+            maxLength="1"
+            className="w-[75px] h-[75px] bg-[#797676] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+          />
+          <input
+            type="text"
+            maxLength="1"
+            className="w-[75px] h-[75px] bg-[#797676] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+          />
+          <input
+            type="text"
+            maxLength="1"
+            className="w-[75px] h-[75px] bg-[#797676] rounded-[10px] border-[#D9D9D9] text-white text-lg text-center"
+          />
+        </div> */}
+
+        <div className="text-sm"> Did not receive the code? Send again.</div>
+
+        <div>
+          <button
+            type="button"
+            className=" bg-[#373A41] px-12 py-2 text-sm rounded  font-Inter"
+          >
+            Verify
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default otpverification;
