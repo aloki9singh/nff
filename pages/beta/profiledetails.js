2e9731157d85df5655697b4788d08c9ebfde8b7e
Yash <bajajy82@gmail.com>
2023-07-06 09:23:06 +0530

    feat: image uploading on chats

--- pages/beta/profiledetails.js
+++ pages/beta/profiledetails.js
@@ -9,45 +9,10 @@ import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { getUserName } from "@/lib/context/AuthContext";
+import { uploadToFirebase } from "@/lib/exportablefunctions";
 
 const options = ["8", "9", "10", "11", "12"];
 
-function uploadToFirebase(file, callback) {
-  const storageRef = ref(storage, "images/" + file.name);
-  const uploadTask = uploadBytesResumable(storageRef, file);
-
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(
-    "state_changed",
-    () => { },
-    (error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case "storage/unauthorized":
-          // User doesn't have permission to access the object
-          break;
-        case "storage/canceled":
-          // User canceled the upload
-          break;
-
-        // ...
-
-        case "storage/unknown":
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    },
-    () => {
-      // Upload completed successfully, now we can get the download URL
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        // console.log('File available at', downloadURL);
-        console.log("downloadURL", downloadURL);
-        callback(downloadURL);
-      });
-    }
-  );
-}
 
 export default function ProfileDetails() {
   const router = useRouter();
