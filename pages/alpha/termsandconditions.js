2c835fa147293686b391f536193487f4a1deda5a
Yash <bajajy82@gmail.com>
2023-07-09 18:51:22 +0530

    updated addcourse page

--- pages/alpha/termsandconditions.js
+++ pages/alpha/termsandconditions.js
@@ -6,7 +6,7 @@
 import Sidebar from '@/components/common/sidebar/sidebar';
 import Dashboardnav from '@/components/common/navbar/dashboardnav';
 import Footer from '@/components/common/footer/footer';
-import BottomNav from "@/components/common/footer/BottomNav";
+import BottomNav from "@/components/common/footer/bottomnav";
 
 export default function Terms() {
   return (
