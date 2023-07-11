56aa76f5120051089cc0ac97346e72c99b1424ce
Yash <bajajy82@gmail.com>
2023-07-11 16:51:51 +0530

    backend for addcourse page

--- pages/reta/addcourse.js
+++ pages/reta/addcourse.js
@@ -17,15 +17,27 @@ import NeatS from "/public/componentsgraphics/schools/login/neatskillslogosample
 import { useAuthContext } from "@/lib/context/AuthContext";
 import { useStep } from "@/hooks/useStep";
 import IDdraganddrop from "@/components/student/assignments/iddraganddrop";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import { uploadToFirebase } from "@/lib/exportablefunctions";
 
 const numOfMentors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const mentorLists = ["Dinesh Saini", "Rahul", "Raj", "Ravi"];
-const numOfModules = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const categories = ["Web Development", "App Development", "UI/UX", "Others"];
 
-const PlanCourseForm = () => {
+const PlanCourseForm = ({ goToNextStep, onSubmit }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    control,
+  } = useForm();
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Header currentStep={1} />
+      <hr className="border-x-2 border-gray-500 mb-4" />
       {/* course name */}
       <div className="w-full flex flex-col md:flex-row justify-start items-start md:items-center gap-y-2 md:gap-x-2 px-4 mb-8">
         <label className="w-40" htmlFor="">
@@ -36,6 +48,7 @@ const PlanCourseForm = () => {
           placeholder="Enter coures title"
           className="AddMentorInput flex-1  h-10 rounded-lg px-2"
           style={{ background: "#333333" }}
+          {...register("title", { required: true })}
         />
       </div>
       <div className="w-full  flex flex-col md:flex-row justify-start items-start  gap-y-2 md:gap-x-2 px-4 mb-8">
@@ -47,6 +60,7 @@ const PlanCourseForm = () => {
           placeholder="Enter course description"
           className="AddMentorInput flex-1 h-28 rounded-lg px-2"
           style={{ background: "#333333" }}
+          {...register("desc", { required: true })}
         />
       </div>
 
@@ -61,6 +75,7 @@ const PlanCourseForm = () => {
             className="AddMentorInput h-10 rounded-lg px-2 flex-1"
             style={{ background: "#333333" }}
             placeholder="Enter duration in weeks"
+            {...register("duration", { required: true, valueAsNumber: true })}
           />
         </div>
         <div className="flex flex-1 items-center gap-x-2 px-4">
@@ -72,6 +87,7 @@ const PlanCourseForm = () => {
             placeholder="Enter total lectures"
             className="AddMentorInput h-10 rounded-lg px-2 flex-1"
             style={{ background: "#333333" }}
+            {...register("lectures", { required: true, valueAsNumber: true })}
           />
         </div>
       </div>
@@ -82,7 +98,13 @@ const PlanCourseForm = () => {
           Level :
         </legend>
         <div className="border-2 border-gray-600 px-3 py-1 rounded-lg">
-          <input type="radio" name="level" value="Beginner" className="mr-2" />
+          <input
+            {...register("level")}
+            type="radio"
+            name="level"
+            value="Beginner"
+            className="mr-2"
+          />
           <label>Beginner</label>
         </div>
         <div className="border-2 border-gray-600 px-3 py-1 rounded-lg">
@@ -91,11 +113,18 @@ const PlanCourseForm = () => {
             name="level"
             value="Intermediate"
             className="mr-2"
+            {...register("level")}
           />
           <label>Intermediate</label>
         </div>
         <div className="border-2 border-gray-600 px-3 py-1 rounded-lg">
-          <input type="radio" name="level" value="Advanced" className="mr-2" />
+          <input
+            {...register("level")}
+            type="radio"
+            name="level"
+            value="Advanced"
+            className="mr-2"
+          />
           <label>Advanced</label>
         </div>
       </div>
@@ -109,8 +138,9 @@ const PlanCourseForm = () => {
             <select
               className="AddMentorInput w-full h-10 bg-[#313131] "
               style={{ background: "#333333" }}
+              {...register("category")}
             >
-              <option className="text-white/50" disabled selected value="">
+              <option className="text-white/50" disabled value="">
                 Select a Category
               </option>
               {categories.map((category) => (
@@ -128,6 +158,7 @@ const PlanCourseForm = () => {
           </label>
           <div className="bg-[#313131] rounded-lg h-10 px-2 flex-1">
             <select
+              {...register("language")}
               className="AddMentorInput w-full h-10 rounded-lg"
               style={{ background: "#313131" }}
             >
@@ -144,81 +175,34 @@ const PlanCourseForm = () => {
         <label className="w-40" htmlFor="">
           Upload Banner Image
         </label>
-        <IDdraganddrop />
+        <Controller
+          control={control}
+          name="banner"
+          render={({ field: { onChange, onBlur } }) => (
+            <IDdraganddrop
+              setValue={setValue}
+              name="banner"
+              onChange={onChange}
+              onBlur={onBlur}
+            />
+          )}
+        />
       </div>
-
-      {/* number of mentor and lead mentor */}
-      {/* <div className="w-full md:w-3/4 flex flex-col md:flex-row justify-start items-start md:items-center gap-y-3 md:gap-x-28 px-4 mb-4 md:mb-8">
-    <div className="flex items-center gap-x-4">
-      <label htmlFor="" className="text-xs md:text-base">
-        Num of Mentor:
-      </label>
-      <div className="bg-[#313131] rounded-lg h-10 px-2">
-        <select
-          className="AddMentorInput w-36 md:w-32 h-10 rounded-lg"
-          style={{ background: "#313131" }}
-          value={numMentor}
-          onChange={(e) => setNumMentor(parseInt(e.target.value))}
-        >
-          <option value="0">0</option>
-          {numOfMentors.map((mentorNum) => (
-            <option key={mentorNum} value={mentorNum}>
-              {mentorNum}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-
-    <div className="flex gap-x-4 items-center">
-      <label htmlFor="" className="text-xs md:text-base">
-        Lead Mentor:
-      </label>
-      <div className="bg-[#313131] rounded-lg h-10 px-2">
-        <select
-          className="AddMentorInput w-48 md:w-60 h-10 rounded-lg"
-          style={{ background: "#313131" }}
-          value={leadMentor}
-          onChange={(e) => setLeadMentor(e.target.value)}
-        >
-          <option value="null">Dinesh Saini</option>
-          {mentorLists.map((mentor) => (
-            <option key={mentor} value={mentor}>
-              {mentor}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-  </div> */}
-
-      {/* assistance mentor */}
-      {/* <div className="border-2 border-gray-500 px-4 py-2 h-30">
-    <div className="flex items-center gap-x-4 mb-4">
-      <label htmlFor="" className="text-xs md:text-base">
-        Assistant Mentor :
-      </label>
-      {renderMentorInputs()}
-    </div>
-    <div className="w-fit md:w-max flex flex-wrap md:flex-nowrap md:flex-row items-center gap-y-2 gap-x-2">
-      {assistMentor.map((mentor) => (
-        <div
-          key={mentor.id}
-          className="w-fit px-2 py-1 border-2 flex items-center gap-x-2 border-gray-500 rounded-lg"
-        >
-          <p>{mentor.name}</p>
-          <IoClose onClick={() => removeMentorInput(mentor.id)} />
-        </div>
-      ))}
-    </div>
-  </div> */}
-    </>
+    </form>
   );
 };
 
-const TargetStudentsForm = () => {
+const TargetStudentsForm = ({ onSubmit }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Header currentStep={2} />
+      <hr className="border-x-2 border-gray-500 mb-4" />
       <div className="w-full flex flex-col mt-12  gap-y-2 md:gap-x-2 px-4 mb-8">
         <label className="" htmlFor="">
           What will the students learn from your course ?
@@ -228,6 +212,7 @@ const TargetStudentsForm = () => {
           placeholder="Example, Will learn basics of UI/UX "
           className="AddMentorInput h-28 max-w-4xl rounded-lg px-2"
           style={{ background: "#333333" }}
+          {...register("learn", { required: true })}
         />
       </div>
       <div className="w-full flex flex-col mt-12  gap-y-2 md:gap-x-2 px-4 mb-8">
@@ -239,6 +224,7 @@ const TargetStudentsForm = () => {
           placeholder="Example, Have a laptop"
           className="AddMentorInput h-28 max-w-4xl rounded-lg px-2"
           style={{ background: "#333333" }}
+          {...register("requirements", { required: true })}
         />
       </div>
       <div className="w-full flex flex-col mt-12  gap-y-2 md:gap-x-2 px-4 mb-8">
@@ -250,9 +236,10 @@ const TargetStudentsForm = () => {
           placeholder="Example, people who are curious about design or beginners"
           className="AddMentorInput h-28 max-w-4xl rounded-lg px-2"
           style={{ background: "#333333" }}
+          {...register("target", { required: true })}
         />
       </div>
-    </>
+    </form>
   );
 };
 
@@ -271,7 +258,7 @@ const headingContent = [
   },
 ];
 
-const Header = ({ currentStep }) => {
+const Header = ({ currentStep, onSubmit }) => {
   return (
     <div className="w-full flex flex-row items-center justify-between p-2 mb-2">
       <div className="flex-[4]">
@@ -283,8 +270,12 @@ const Header = ({ currentStep }) => {
         </p>
       </div>
       <div className="flex-1 text-right">
-        <button className="px-12 py-3 bg-[#A145CD] rounded-md hover:scale-105 duration-100 transition-all">
-          Next
+        <button
+          onClick={() => onSubmit?.()}
+          type="submit"
+          className="px-12 py-3 bg-[#A145CD] rounded-md hover:scale-105 duration-100 transition-all"
+        >
+          {currentStep === 3 ? "Submit" : "Next"}
         </button>
       </div>
     </div>
@@ -337,7 +328,7 @@ const Accordian = ({ title, desc }) => {
   );
 };
 
-const CourseContentForm = () => {
+const CourseContentForm = ({ onSubmit }) => {
   const [modules, setModules] = useState([]);
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
@@ -350,63 +341,67 @@ const CourseContentForm = () => {
   };
 
   return (
-    <div className="w-full flex flex-row itesm-start gap-5">
-      <form
-        onSubmit={addModuleHandler}
-        className="flex-1 px-10 py-6  border border-[#5F6065] rounded-xl"
-      >
-        <div className="w-full flex flex-col gap-y-4 mb-7">
-          <label className="" htmlFor="">
-            Module Name
-          </label>
-          <input
-            type="text"
-            placeholder="Enter module name"
-            className="AddMentorInput  rounded-lg px-2"
-            style={{ background: "#333333" }}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="w-full flex flex-col gap-y-4 mb-7">
-          <label className="" htmlFor="">
-            Module Description
-          </label>
-          <textarea
-            type="text"
-            placeholder="Enter module description"
-            className="AddMentorInput h-60 max-w-4xl rounded-lg px-2"
-            style={{ background: "#333333" }}
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-          />
-        </div>
-        <button className="bg-pink text-white px-10 py-2 rounded-md ">
-          Add
-        </button>
-      </form>
-      <div className="flex-1 flex flex-col ">
-        <h5>Class module list</h5>
-        <div className="flex-1">
-          {modules.length === 0 ? (
-            <div className="flex h-full items-center justify-center">
-              <p className="text-center text-white/60 text-lg">
-                No added module
-              </p>
-            </div>
-          ) : (
-            <div className="flex flex-col mt-4 gap-y-3">
-              {modules.map((module, index) => (
-                <div key={index}>
-                  <p className="text-xs text-white/70">Module {index + 1}</p>
-                  <Accordian title={module.name} desc={module.desc} />
-                </div>
-              ))}
-            </div>
-          )}
+    <>
+      <Header currentStep={3} onSubmit={() => onSubmit(modules)} />
+      <hr className="border-x-2 border-gray-500 mb-4" />
+      <div className="w-full flex flex-row itesm-start gap-5">
+        <form
+          onSubmit={addModuleHandler}
+          className="flex-1 px-10 py-6  border border-[#5F6065] rounded-xl"
+        >
+          <div className="w-full flex flex-col gap-y-4 mb-7">
+            <label className="" htmlFor="">
+              Module Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter module name"
+              className="AddMentorInput  rounded-lg px-2"
+              style={{ background: "#333333" }}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="w-full flex flex-col gap-y-4 mb-7">
+            <label className="" htmlFor="">
+              Module Description
+            </label>
+            <textarea
+              type="text"
+              placeholder="Enter module description"
+              className="AddMentorInput h-60 max-w-4xl rounded-lg px-2"
+              style={{ background: "#333333" }}
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </div>
+          <button className="bg-pink text-white px-10 py-2 rounded-md ">
+            Add
+          </button>
+        </form>
+        <div className="flex-1 flex flex-col ">
+          <h5>Class module list</h5>
+          <div className="flex-1">
+            {modules.length === 0 ? (
+              <div className="flex h-full items-center justify-center">
+                <p className="text-center text-white/60 text-lg">
+                  No added module
+                </p>
+              </div>
+            ) : (
+              <div className="flex flex-col mt-4 gap-y-3">
+                {modules.map((module, index) => (
+                  <div key={index}>
+                    <p className="text-xs text-white/70">Module {index + 1}</p>
+                    <Accordian title={module.name} desc={module.desc} />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -416,7 +411,7 @@ const Sidebar = ({ currentStep = 1, setStep }) => {
   return (
     <div className="mt-8 flex flex-col items-stretch gap-5">
       {steps.map((step, index) => (
-        <buttton
+        <button
           onClick={() => setStep(index + 1)}
           className="py-4 group cursor-pointer transition-colors duration-150"
           key={index}
@@ -437,80 +432,84 @@ const Sidebar = ({ currentStep = 1, setStep }) => {
           >
             {step}
           </p>
-        </buttton>
+        </button>
       ))}
     </div>
   );
 };
 
+const createCourse = async (courseDetails) => {
+
+  const data = {
+    ...courseDetails,
+    createdAt: serverTimestamp(),
+    banner: "",
+  }
+
+  console.log("data final", data)
+
+  const courseRef = await addDoc(collection(db, "courses"),data);
+
+  if (courseDetails.banner) {
+    uploadToFirebase(courseDetails.banner, (url) => {
+      setDoc(
+        doc(db, "courses", courseRef.id),
+        {
+          banner: url,
+          id: courseRef.id,
+        },
+        {
+          merge: true,
+        }
+      );
+    });
+  }
+
+  console.log("courseRef", courseRef.id);
+
+  return courseRef.id;
+};
+
 const CreateCourse = () => {
-
-  // const [numMentor, setNumMentor] = useState(0);
-  // const [leadMentor, setLeadMentor] = useState("");
-  // const [assistMentor, setAssistMentor] = useState([
-  //   { id: 1, name: "Dinesh Saini" },
-  //   { id: 2, name: "Rahul" },
-  //   { id: 3, name: "Raj" },
-  //   { id: 4, name: "Ravi" },
-  // ]);
-
-  // const handleAssistanceMentorChange = (index, field, value) => {
-  //   const updatedAssistanceMentors = [...assistMentor];
-  //   updatedAssistanceMentors[index] = {
-  //     ...updatedAssistanceMentors[index],
-  //     [field]: value,
-  //   };
-  //   updatedAssistanceMentors[index] = {
-  //     ...updatedAssistanceMentors[index],
-  //     id: index + 1,
-  //   };
-  //   setAssistMentor(updatedAssistanceMentors);
-  // };
-
-
-  // const renderMentorInputs = () => {
-  //   const assistanceMentorInputs = Array.from(
-  //     { length: numMentor - 1 },
-  //     (_, i) => {
-  //       const assistanceMentor = assistMentor[i] || {
-  //         id: i + 1,
-  //         name: "",
-  //       };
-
-  //       return (
-  //         <div key={i}>
-  //           <input
-  //             type="text"
-  //             value={assistanceMentor.name}
-  //             placeholder="Assistant Mentor"
-  //             className="AddMentorInput w-1/2 md:w-3/4 h-10 rounded-xl px-2"
-  //             onChange={(e) =>
-  //               handleAssistanceMentorChange(i, "name", e.target.value)
-  //             }
-  //             style={{ background: "#333333" }}
-  //           />
-  //         </div>
-  //       );
-  //     }
-  //   );
-  //   return assistanceMentorInputs;
-  // };
-
-  // function removeMentorInput(id) {
-  //   console.log("removed");
-  //   setAssistMentor(assistMentor.filter((m) => m.id !== id));
-  // }
-
   const [currentStep, helpers] = useStep(3);
+  const [formData, setFormData] = useState({});
+  const router = useRouter();
 
-  const {
-    canGoToPrevStep,
-    canGoToNextStep,
-    goToNextStep,
-    goToPrevStep,
-    reset,
-    setStep,
-  } = helpers;
+  const { goToNextStep, goToPrevStep, setStep } = helpers;
+
+  console.log("formData", formData);
+
+  const onPlanCourseFormSubmit = (data) => {
+    setFormData({ ...formData, ...data });
+    console.log("formData", data);
+    goToNextStep();
+  };
+
+  const onTargetStudentFormSubmit = (data) => {
+    setFormData({
+      ...formData,
+      "Q&A": {
+        ...data,
+      },
+    });
+
+    console.log("target students data", data);
+    goToNextStep();
+  };
+
+  const onCourseContentFormSubmit = async (modules) => {
+    const courseDetails = {
+      ...formData,
+      modules,
+    };
+    console.log("modules", modules);
+
+    setFormData(courseDetails);
+
+    await createCourse(courseDetails);
+
+    router.push("beta/course-overview");
+  };
 
   return (
     <div className="text-white flex flex-col justify-center items-center px-5">
@@ -557,15 +556,16 @@ const CreateCourse = () => {
           <Sidebar currentStep={currentStep} setStep={setStep} />
         </div>
         <div className="col-span-8 h-max  p-8 bg-[#222222] rounded-lg mt-4 mb-4">
-          <Header currentStep={currentStep} />
-          <hr className="border-x-2 border-gray-500 mb-4" />
+          {currentStep === 1 && (
+            <PlanCourseForm onSubmit={onPlanCourseFormSubmit} />
+          )}
 
-          {currentStep === 1 && <PlanCourseForm />}
-
-          {currentStep === 3 && <CourseContentForm />}
+          {currentStep === 3 && (
+            <CourseContentForm onSubmit={onCourseContentFormSubmit} />
+          )}
 
           {currentStep === 2 && (
-            <TargetStudentsForm />
+            <TargetStudentsForm onSubmit={onTargetStudentFormSubmit} />
           )}
         </div>
       </div>
