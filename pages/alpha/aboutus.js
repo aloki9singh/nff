61b13e8b5a116988cd273e86ce2834b448eb2133
MIRAGE <mthakore12@gmail.com>
2023-07-12 20:12:31 +0530

    about uspage full done just image svg in not proper

--- pages/alpha/aboutus.js
+++ pages/alpha/aboutus.js
@@ -1,15 +1,35 @@
+import React, { useRef } from 'react';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+
 import NavbarSecond from '@/components/common/navbar/navbar2';
 import Sidebar from '@/components/common/sidebar/sidebar';
 import Image from 'next/image';
 import { useState } from 'react';
 import { contactFn } from '@/lib/api';
 import { Carousel } from 'react-responsive-carousel';
-import Team1 from '@/public/componentsgraphics/common/aboutpage/team1.png';
+
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Footer from '@/components/common/footer/footer';
 import CarouselComp from '@/components/common/carousel/carousel';
+import Image1 from '../../public/componentsgraphics/common/aboutpage/team1.svg';
 
 const Aboutus = () => {
+  const scrollContainerRef = useRef(null);
+
+  const scrollBackward = () => {
+    scrollContainerRef.current.scrollBy({
+      left: -200, // Adjust the scroll amount as per your requirements
+      behavior: 'smooth'
+    });
+  };
+
+  const scrollForward = () => {
+    scrollContainerRef.current.scrollBy({
+      left: 200, // Adjust the scroll amount as per your requirements
+      behavior: 'smooth'
+    });
+  };
+
   const style = `
   .hide-scrollbar::-webkit-scrollbar {
     display: none;
@@ -34,47 +54,25 @@ const Aboutus = () => {
     { width: 1200, itemsToShow: 4 }
   ];
 
-  const mentors = [
-    {
-      id: 1,
-      name: 'Preeti Pathak',
-      designation: 'team player 1'
-    },
-    {
-      id: 2,
-      name: 'Preeti Pathak',
-      designation: 'team player 2'
-    },
-    {
-      id: 3,
-      name: 'Preeti Pathak',
-      designation: 'team player 3'
-    },
-    {
-      id: 4,
-      name: 'Preeti Pathak',
-      designation: 'team player 4'
-    }
-  ];
-
   return (
     <>
       <NavbarSecond buttonVis='hidden' title='About Us' />
       <div className='h-full'>
-        <div className='w-full h-full flex flex-col overflow-auto'>
+        <div className='w-full h-full flex flex-col'>
           <div className='flex justify-center items-center flex-col'>
             <span className='text-white text-4xl mt-14'>
-              we&apos;re here to
+              We&apos;re here to
             </span>
-            <span className='text-[#E1348B] text-4xl mt-5 text-center decoration-white ml-5'>
+            <span className=' text-4xl mt-5 text-center bg-gradient-to-r from-[#A134CD] to-[#E1348B] bg-clip-text  text-transparent   '>
               guarantee your success
             </span>
+            <div className='w-80 h-1 bg-white mt-2 ml-4' />
           </div>
 
-          <div className='w-[80%] h-64'>
+          <div className='w-[80%] h-auto'>
             <div className='relative'>
-              <div className='w-full h-[70%] ml-10 md:ml-44 mt-11 p-5 rounded-md bg-gradient-to-b from-[#A134CD] to-[#E1348B] flex items-center text-center text-white font-semibold overflow-auto'>
-                <span className='text-sm md:text-sm lg:text-2xl'>
+              <div className='w-full h-56 ml-10 md:ml-44 mt-11 p-5 rounded-md bg-gradient-to-b from-[#A134CD] to-[#E1348B] flex items-center text-center text-white font-semibold overflow-auto'>
+                <span className='text-sm md:text-sm lg:text-xl'>
                   ConsultUs provides consulting services that help business
                   owners and leaders build a more valuable business. We worked
                   with their founder to build a professional, modern site that
@@ -83,14 +81,20 @@ const Aboutus = () => {
                 </span>
               </div>
             </div>
-            <div className='w-[75%] bg-white mx-auto mt-5 md:ml-80'>
-              {/* team image */}
+            <div class='relative'>
+              <div class='absolute w-full  h-56  ml-10 md:ml-44  -bottom-48 rounded-lg overflow-hidden'>
+                <img
+                  src='https://source.unsplash.com/random/350x350'
+                  alt='random image'
+                  class='w-full object-center px-8 shadow-md transform -translate-y-16'
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className='w-[80%] h-auto mt-20 ml-10 md:ml-44 mb-10'>
-          <span className='text-lg ml-4 text-white'>Our</span>
-          <span className='text-lg ml-2 bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
+        <div className='w-[80%] h-auto  mt-56 ml-10 md:ml-44 mb-10'>
+          <span className='text-xl  font-extrabold ml-4 text-white'>Our</span>
+          <span className='text-xl  font-extrabold ml-2 bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
             mission
           </span>
           <div className='w-64 h-1 bg-white mt-2 ml-4' />
@@ -131,79 +135,139 @@ const Aboutus = () => {
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Our mission */}
-      <div className='w-[80%] h-auto mt-10 ml-10 md:ml-44 mb-10'>
-        <span className='text-lg ml-4 text-white'>Our</span>
-        <span className='text-lg ml-2 bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
-          Commitment
-        </span>
-        <div className='w-64 h-1 bg-white mt-2 ml-4' />
+        {/* Our mission */}
+        <div className='w-[80%] h-auto mt-10 ml-10 md:ml-44 mb-10'>
+          <span className='text-xl font-extrabold ml-4 text-white'>Our</span>
+          <span className='text-xl font-extrabold ml-2 bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
+            Commitment
+          </span>
+          <div className='w-64 h-1 bg-white mt-2 ml-4' />
 
-        <div className='flex flex-col md:flex-row p-4'>
-          <div className='text-white mb-4 md:mb-0 md:mr-8'>Ethics</div>
-          <div className='text-white ml-0 md:ml-20'>
-            Ethics for ConsultUs means ensuring our customers the
-            confidentiality and uniqueness of the service they are provided. A
-            client must be reassured by the fact that the service provided to
-            them is made-to-measure and will not be recycled for their
-            competitors.
+          <div className='flex flex-col md:flex-row p-4'>
+            <div className='text-white mb-4 md:mb-0 md:mr-8'>Ethics</div>
+            <div className='text-white ml-0 md:ml-32'>
+              Ethics for ConsultUs means ensuring our customers the
+              confidentiality and uniqueness of the service they are provided. A
+              client must be reassured by the fact that the service provided to
+              them is made-to-measure and will not be recycled for their
+              competitors.
+            </div>
+          </div>
+
+          <div className='flex flex-col md:flex-row p-4'>
+            <div className='text-white mb-4 md:mb-0 md:mr-8'>Quality</div>
+            <div className='text-white ml-0 md:ml-28 md:pl-1'>
+              ConsultUs is committed to ensuring that our advice and
+              recommendations are based on the best combination of methods,
+              information research, creativity and internal quality assurance.
+            </div>
+          </div>
+
+          <div className='flex flex-col md:flex-row p-4'>
+            <div className='text-white mb-4 md:mb-0 md:mr-8'>Continuity</div>
+            <div className='text-white ml-0 md:ml-24 mr-2'>
+              ConsultUs considers that the continuity of relations on the long
+              term with its clients is the guarantee of the satisfaction of
+              these and the quality of the services provided.
+            </div>
           </div>
         </div>
 
-        <div className='flex flex-col md:flex-row p-4'>
-          <div className='text-white mb-4 md:mb-0 md:mr-8'>Quality</div>
-          <div className='text-white ml-0 md:ml-16 md:pl-1'>
-            ConsultUs is committed to ensuring that our advice and
-            recommendations are based on the best combination of methods,
-            information research, creativity and internal quality assurance.
-          </div>
-        </div>
+        {/* ourteam section */}
+        {/* Our Team */}
+        <div class='container mx-auto w-screen flex-grow px-0 px-4 py-4 sm:py-16'>
+          <h1 class='mx-auto mb-4  px-2 text-center text-3xl uppercase bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
+            our successful team
+          </h1>
+          <div class='mx-auto w-auto  md:w-auto'>
+            <div class='container my-8'>
+              <div
+                ref={scrollContainerRef}
+                class='flex-no-wrap scrolling-touch mb-8 flex items-start  overflow-x-hidden'>
+                <div class='mr-8  max-w-fit flex-none rounded-lg   md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team1.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
 
-        <div className='flex flex-col md:flex-row p-4'>
-          <div className='text-white mb-4 md:mb-0 md:mr-8'>Continuity</div>
-          <div className='text-white ml-0 md:ml-10'>
-            ConsultUs considers that the continuity of relations on the long
-            term with its clients is the guarantee of the satisfaction of these
-            and the quality of the services provided.
-          </div>
-        </div>
-      </div>
+                <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team2.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
 
-      {/* ourteam section */}
-      {/* Our Team */}
-      <div class='container mx-auto w-full flex-grow px-0 px-4 py-4 sm:py-16'>
-        <h1 class='mx-auto mb-4  px-2 text-center text-3xl uppercase bg-gradient-to-r from-[#A134CD] to-[#E1348B] text-transparent bg-clip-text'>
-          our successful team
-        </h1>
-        <div class='mx-auto w-full px-4 md:w-4/5'>
-          <div class='container my-8'>
-            <div
-              id='scrollContainer'
-              class='flex-no-wrap scrolling-touch mb-8 flex items-start overflow-x-scroll hide-scrollbar'>
-              <div class='mr-8  max-w-fit flex-none rounded-lg border  md:pb-4'>
-                <a href='#' class='space-y-4'>
-                  <div class='aspect-w-max aspect-h-ma'>
-                    <img
-                      class='rounded-lg object-cover shadow-md hover:shadow-xl'
-                      src='/componentsgraphics/mentor/mentorcard/mentorpfpsample.svg'
-                      alt=''
-                    />
-                  </div>
-                </a>
+                <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team3.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
+
+                <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team1.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
+
+                <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team2.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
+
+                <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
+                  <a href='#' class='space-y-4'>
+                    <div class='aspect-w-max aspect-h-ma'>
+                      <img
+                        class='rounded-lg object-cover shadow-md hover:shadow-xl'
+                        src='/componentsgraphics/common/aboutpage/team3.svg'
+                        alt=''
+                      />
+                    </div>
+                  </a>
+                </div>
               </div>
-
-              <div class='mr-8 max-w-fit flex-none rounded-lg border  md:pb-4'>
-                <a href='#' class='space-y-4'>
-                  <div class='aspect-w-max aspect-h-ma'>
-                    <img
-                      class='rounded-lg object-cover shadow-md hover:shadow-xl'
-                      src='/componentsgraphics/mentor/mentorcard/mentorpfpsample.svg'
-                      alt=''
-                    />
-                  </div>
-                </a>
+              <div className='flex  item-center justify-center  gap-4  items-center mx-auto max-w-screen-md px-4'>
+                <button
+                  onClick={scrollBackward}
+                  className='p-2 rounded-full bg-white shadow-md hover:shadow-lg focus:outline-none'>
+                  <BsChevronLeft size={24} />
+                </button>
+                <button
+                  onClick={scrollForward}
+                  className='p-2 rounded-full bg-white shadow-md hover:shadow-lg focus:outline-none'>
+                  <BsChevronRight size={24} color='black' />
+                </button>
               </div>
             </div>
           </div>
@@ -227,7 +291,7 @@ const Aboutus = () => {
                     <input
                       type='email'
                       id='email'
-                      className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      class='shadow-sm bg-transparent border-0 border-b border-gray-300 text-gray-900 text-sm rounded-none focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-transparent dark:border-0 dark:border-b dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                       placeholder='name@flowbite.com'
                       required
                     />
@@ -241,7 +305,7 @@ const Aboutus = () => {
                     <input
                       type='text'
                       id='subject'
-                      className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      class='shadow-sm bg-transparent border-0 border-b border-gray-300 text-gray-900 text-sm rounded-none focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-transparent dark:border-0 dark:border-b dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                       placeholder='Let us know how we can help you'
                       required
                     />
@@ -256,7 +320,7 @@ const Aboutus = () => {
                     <input
                       type='text'
                       id='subject'
-                      className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      class='shadow-sm bg-transparent border-0 border-b border-gray-300 text-gray-900 text-sm rounded-none focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-transparent dark:border-0 dark:border-b dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                       placeholder='Let us know how we can help you'
                       required
                     />
@@ -271,7 +335,7 @@ const Aboutus = () => {
                     <input
                       type='text'
                       id='subject'
-                      className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      class='shadow-sm bg-transparent border-0 border-b border-gray-300 text-gray-900 text-sm rounded-none focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-transparent dark:border-0 dark:border-b dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                       placeholder='Let us know how we can help you'
                       required
                     />
@@ -286,7 +350,7 @@ const Aboutus = () => {
                     <input
                       type='text'
                       id='subject'
-                      className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                      class='shadow-sm bg-transparent border-0 border-b border-gray-300 text-gray-900 text-sm rounded-none focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-transparent dark:border-0 dark:border-b dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                       placeholder='Let us know how we can help you'
                       required
                     />
