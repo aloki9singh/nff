a8a717a826626fdf5a73d2e7382eb02df4c22edf
aloksingh1st <aloksingh1st@gmail.com>
2023-07-18 20:47:46 +0530

    button removed form congrats page and adding modify course page in mentor section

--- pages/meta/modifyCourses.js
+++ pages/meta/modifyCourses.js
@@ -153,39 +153,3 @@ export default function modifyCourses() {
   )
 }
 
-
-
-
-export async function getStaticProps() {
-  try {
-    const CoursesCollectionref = collection(db, "courses");
-    const coursesSnapshot = await getDocs(CoursesCollectionref);
-    const coursesData = JSON.stringify(coursesSnapshot.docs.map((doc) => {
-      return ({
-        id: doc.id,
-        title: doc.data().title,
-        desc: doc.data().desc,
-        level: doc.data().level,
-        sessions: doc.data().lectures,
-        language: doc.data().language,
-        category: doc.data().category,
-        banner: doc.data().banner
-      })
-    }));
-    return {
-      props: {
-        coursesData,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        coursesData: [],
-      },
-    };
-  }
-}
-
-
-
