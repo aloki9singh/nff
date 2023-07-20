e95cf9a3ca12282d70805929e5a0011e42ebcafc
Yash <bajajy82@gmail.com>
2023-07-20 12:09:21 +0530

    added edit/modify course feature

--- pages/reta/addcourse/congrats.js
+++ pages/reta/addcourse/congrats.js
@@ -31,38 +31,38 @@ const CongratsAddTeam = () => {
   return (
     <>
       <div className='w-screen h-screen flex items-center justify-center'>
-      <div
-        className='d-flex items-center justify-center w-[85%] sm:w-[1350px] h-[724px] rounded-xl bg-[#1E1E1E] m-12'
-        style={{
-          backgroundImage: "url('/pagesgraphics/admin/AddTeam/Frame.svg')",
-          display: 'flex'
-        }}>
         <div
-          className='block max-w-2xl py-8 px-20  rounded-lg shadow '
-          style={glass}>
-          <div className='d-flex justify-center'>
-            <div className='text-container text-center'>
-              <h5 class='mb-3 text-4xl font-bold tracking-tight text-white dark:text-white'>
-                Congratulations
-              </h5>
-              <h6 className='text-white font-normal text-lg max-w-lg'>
-              A new course has successfully been added. The course will only be visible to public after assigning a mentor to it.
-              </h6>
-            </div>
+          className='d-flex items-center justify-center w-[85%] sm:w-[1350px] h-[724px] rounded-xl bg-[#1E1E1E] m-12'
+          style={{
+            backgroundImage: "url('/pagesgraphics/admin/AddTeam/Frame.svg')",
+            display: 'flex'
+          }}>
+          <div
+            className='block max-w-2xl py-8 px-20  rounded-lg shadow '
+            style={glass}>
+            <div className='d-flex justify-center'>
+              <div className='text-container text-center'>
+                <h5 class='mb-3 text-4xl font-bold tracking-tight text-white dark:text-white'>
+                  Congratulations
+                </h5>
+                <h6 className='text-white font-normal text-lg max-w-lg'>
+                  A new course has successfully been added. The course will only be visible to public after assigning a mentor to it.
+                </h6>
+              </div>
 
-            <div className='button-container text-center my-3 '>
-              <button
-                className='d-flex text-white sm:m-4 h-11 px-6 justify-center items-center gap-2 rounded-xl bg-[#E1348B] my-4'
-                // onClick={() => router.push('/meta/dashboard')}
-                onClick={()=> router.push({pathname: "/meta/dashboard", query:{uid: e.uid}})}
-                
+              <div className='button-container text-center my-3 '>
+                <button
+                  className='d-flex text-white sm:m-4 h-11 px-6 justify-center items-center gap-2 rounded-xl bg-[#E1348B] my-4'
+                  // onClick={() => router.push('/meta/dashboard')}
+                  onClick={() => router.push({ pathname: "/meta/dashboard", })}
+
                 >
-                Go to Dashboard
-              </button>
+                  Go to Dashboard
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
