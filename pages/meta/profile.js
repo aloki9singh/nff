c8ce88348cb46fd1b94fb0f90d3879123ada846d
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-07-22 18:25:20 +0530

    some changes in edit profile need to be done

--- pages/meta/profile.js
+++ pages/meta/profile.js
@@ -3,11 +3,11 @@
 import { useState } from "react";
 // import MentorSidebar from "../components/Schedule/MentorSidebar2";
 
-import CourseCard from "@/components/student/courses/CourseCard";
+import CourseCard from "@/components/student/courses/CourseCard2";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { callUserById } from "@/lib/exportablefunctions";
-import { auth } from "@/config/firebaseconfig";
+import { auth, db } from "@/config/firebaseconfig";
 import { onAuthStateChanged } from "firebase/auth";
 
 import withAuth from "@/lib/context/mentorcontext";
@@ -21,6 +21,7 @@ import { FiEdit2 } from "react-icons/fi";
 import { useMediaQuery } from "react-responsive";
 import withMentorAuthorization from "@/lib/HOC/withMentorAuthorization.js";
 import Link from "next/link";
+import { collection, getDocs, query } from "firebase/firestore";
 
 function MentorProfile() {
   const router = useRouter();
@@ -33,6 +34,20 @@ function MentorProfile() {
   const [showSideBar, setShowSideBar] = useState(false);
   const [SideBarState, sendSideBarState] = useState(false);
   const [rating, setRating] = useState(0);
+  const [myCourse, setMyCourse] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, "courses"));
+      const myCourseSnapshot = await getDocs(q);
+      const myCourseData = myCourseSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMyCourse(myCourseData);
+    };
+    fetchData();
+  }, []);
   const [answeredquestions, setAnsweredQuestions] = useState(0);
   function toggleSideBar() {
     setShowSideBar(!showSideBar);
@@ -88,7 +103,7 @@ function MentorProfile() {
               {/* text */}
               <div className="h-[120px] w-full bg-gradient-to-r from-[#A145CD] to-[#E1348B] " />
               <div className="w-[90%] h-full   md:text-base text-sm  ">
-                <div className=" md:mx-10 mx-5">
+                <div className="  mx-5">
                   <div className="flex">
                     {" "}
                     <Image
@@ -119,7 +134,8 @@ function MentorProfile() {
                         }
                       >
                         <div className="flex text-xs md:text-sm mt-[-25px]">
-                          Edit profile
+                          <div className="md:hidden">Edit profile</div>
+                          <div className="md:block hidden">Edit profile</div>
                           <FiEdit2 className="ml-1 mt-[2px]" />
                         </div>
                       </div>
@@ -129,42 +145,44 @@ function MentorProfile() {
               </div>
             </div>
             {/* /// */}
-            <div className="flex max-[963px]:flex-wrap max-[963px]:justify-start  justify-center mx-auto w-[95%] m-5  md:mt-0 text-white ">
-              <div className="lg:pr-4 w-[70%] max-[963px]:w-[98%] ">
-                <div className="lg:flex gap-3 justify-around mt-10 mb-5">
-                  <div className=" lg:w-1/4">
+            <div className="flex md:flex-row flex-col    justify-center  w-[95%] m-5  md:mt-0 text-white ">
+              <div className="lg:pr-4 ">
+                <div className="lg:flex gap-2 justify-around mt-10 mb-5">
+                  <div className=" lg:w-1/4 ">
                     <div>
                       <div className="text-left font-semibold text-base ml-1">
                         Stats.
                       </div>
-                      <div className="text-center gap-5  text-white  my-1 flex lg:block">
-                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 my-4 ">
+                      <div className="text-center gap-5  text-white  my-1 flex md:flex-row flex-col lg:block">
+                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 md:my-4 ">
                           <p className="text-sm font-semibold">{rating}/5.0</p>
-                          <p className="text-xs font-medium">Tutor rating</p>
+                          <p className="text-xs font-medium whitespace-nowrap">
+                            Tutor rating
+                          </p>
                         </div>
-                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 my-4">
+                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 md:my-4">
                           <p className="text-sm font-semibold">
                             {answeredquestions}
                           </p>
-                          <p className="text-xs font-medium">
+                          <p className="text-xs font-medium whitespace-nowrap">
                             Question answered
                           </p>
                         </div>
-                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 my-4">
+                        <div className="bg-gradient-to-r from-[#A145CD] to-[#E1348B] rounded-2xl   p-3 md:my-4">
                           <p className="text-sm font-semibold">August 2023</p>
-                          <p className="text-xs font-medium">
+                          <p className="text-xs font-medium whitespace-nowrap">
                             Material Prepared
                           </p>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="">
+                  <div className=" md:w-3/4 h-[50vh] ">
                     <div className="text-left">
                       <div className="md:ml-1 font-semibold text-base">
                         Number of students
                       </div>
-                      <div className="mt-5 md:mt-2 ">
+                      <div className="mt-5 md:mt-2  ">
                         <MentorChart data={chartData} />
                       </div>
                     </div>
@@ -173,75 +191,64 @@ function MentorProfile() {
                 <div>
                   <div className="flex justify-between m-5 md:mx-0 my-2">
                     <div>Top courses</div>
-                    <div>
+                    {/* <div>
                       <button className="border-2 p-1 text-sm">View All</button>
-                    </div>
+                    </div> */}
                   </div>
-                  <div className="flex overflow-x-scroll scrollbar-hide gap-4">
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
-                    <div>
-                      <CourseCard
-                        key="1"
-                        lessons="8"
-                        title="Introduction to C++"
-                        desc="Learn the basics of C++ programming language."
-                        level="Beginner"
-                        icon="/pagesgraphics/mentor/profile/ProgrammingIcon.svg"
-                      />
-                    </div>
+
+                  <div className="w-[100%] flex-wrap md:grid md:grid-cols-3 rounded-[30px] md:space-y-0 space-y-5  gap-3 pt-5">
+                    {myCourse &&
+                      myCourse.slice(0, 3).map((e) => (
+                        <div
+                          className="border bg-black rounded-[30px] p-5 md:py-5 "
+                          key={e.id}
+                        >
+                          <div className="flex ">
+                            <div className="my-auto">
+                              <Image
+                                width={110}
+                                height={110}
+                                alt={"img"}
+                                src={e.banner}
+                                className="w-12 h-12 md:w-10"
+                              />
+                            </div>
+                            <div className="text-xs md:text-[10px] text-[#E1348B] m-auto">
+                              {e.lectures} Lessons
+                            </div>
+                            <div className="text-xs md:text-[10px] md:ml-1 text-[#E1348B] m-auto">
+                              {e.level}
+                            </div>
+                          </div>
+                          <div className="text-[10px] text-gray-600">
+                            COURSE
+                          </div>
+                          <h1 className="text-sm overflow-hidden overflow-ellipsis md:line-clamp-1">
+                            {e.title}
+                          </h1>
+                          <p className="text-[13px] md:text-[10px] md:w-auto w-[300px] whitespace-break-spaces leading-4 text-gray-500 overflow-hidden overflow-ellipsis md:line-clamp-2 line-clamp-3">
+                            {e.desc}
+                          </p>
+
+                          <div className="text-right">
+                            <Link
+                              href={{
+                                pathname: "/beta/coursedetail",
+                                query: { title: e.title },
+                              }}
+                            >
+                              <button className="mt-2 text-xs  border-pink p-2 border">
+                                Explore
+                              </button>
+                            </Link>
+                          </div>
+                        </div>
+                      ))}
                   </div>
                 </div>
               </div>
-              <div className="">
+             <div className="md:mt-12">
+               <div className="">
                 <div className="bg-[#373A41] rounded-[20px] text-xl justify-center pb-9 pt-3 px-4 space-y-2 mt-5 flex flex-col">
                   <div className=" text-center text-xl font-bold text-gray-500">
                     Educator highlights
@@ -251,7 +258,7 @@ function MentorProfile() {
                     {userData.details &&
                       userData.details?.experience[0]?.companyname}
                   </div>
-                  <div className="flex gap-2 text-lg font-medium">
+                  <div className="flex gap-2 text-sm font-medium line-clamp-1 text-ellipsis">
                     {" "}
                     <span>
                       <Image
@@ -263,7 +270,7 @@ function MentorProfile() {
                       />{" "}
                     </span>{" "}
                     Studied at{" "}
-                    <span className="text-[#E1348B] text-lg font-black">
+                    <span className="text-[#E1348B] text-sm font-black line-clamp-1 text-ellipsis">
                       {userData.details &&
                         userData.details?.qualification[0]?.universityname}
                       (
@@ -272,11 +279,14 @@ function MentorProfile() {
                       )
                     </span>
                   </div>
+
                   <p className="ml-7  font-extralight text-[0.9rem] opacity-75">
-                    Latest Result: Ashwin - 100%ile Thrice in Maths in JEE Main
-                    2021 , AIR 409 (JEE Advanced) through my Evolve Batch. Many
-                    Students Scoring more than 99.5%ile in Maths. Producing
-                    IITians every year.
+                    {userData?.details?.qualification.map((e) => (
+                      <div key={e}>
+                        {e?.universityname} {e.fieldOfStudy}
+                        {e.cgpa} {e.grade}
+                      </div>
+                    ))}
                   </p>
 
                   <div className="flex gap-2 text-[0.9rem] font-medium">
@@ -291,12 +301,12 @@ function MentorProfile() {
                       />{" "}
                     </span>{" "}
                     Lives in{" "}
-                    <span className="text-[#E1348B] font-black">
+                    <span className="text-[#E1348B] font-black line-clamp-1 text-ellipsis">
                       {" "}
                       {userData.details?.address}
                     </span>
                   </div>
-                  <p className="ml-7  font-extralight text-[0.9rem] opacity-75">
+                  <p className="ml-7  font-extralight text-[0.9rem] opacity-75 line-clamp-2 text-ellipsis">
                     {userData.details &&
                       userData.details?.experience[0]?.jobtitle}{" "}
                     since{" "}
@@ -304,28 +314,29 @@ function MentorProfile() {
                       userData.details?.experience[0]?.startdate}
                   </p>
 
-                  <div className="flex gap-2">
+                  <div className="flex gap-2 line-clamp-1 text-ellipsis">
                     {" "}
-                    <span>
-                      <Image
-                        src={"/pagesgraphics/mentor/profile/globe_icon.svg"}
-                        width={100}
-                        height={100}
-                        alt="img"
-                        className="w-5"
-                      />{" "}
-                    </span>{" "}
-                    <span className="text-[0.9rem] font-medium">
+                    <Image
+                      src={"/pagesgraphics/mentor/profile/globe_icon.svg"}
+                      width={100}
+                      height={100}
+                      alt="img"
+                      className="w-5 h-5"
+                    />{" "}
+                    <span className="text-[0.9rem] font-medium flex">
                       {" "}
                       Knows{" "}
-                      {userData.details &&
-                        userData.details?.skills.map((item) => {
-                          return <>{item},</>;
-                        })}
+                      <span className="text-[#E1348B] font-black  pl-2 line-clamp-2 text-ellipsis">
+                        {userData.details &&
+                          userData.details?.skills.map((item) => {
+                            return <span key={item}>{item}</span>;
+                          })}
+                      </span>
                     </span>
                   </div>
                 </div>
               </div>
+             </div>
             </div>
           </div>
         </div>
