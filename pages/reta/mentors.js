eb7d7c82299990c1ba99a21d0a0c88402662bbfd
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-08-04 18:44:48 +0530

    added set up

--- pages/reta/mentors.js
+++ pages/reta/mentors.js
@@ -140,6 +140,7 @@ function AdminStudent() {
 
   useEffect(() => {
     getTotalPages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const updateButtonStatus = (isActive) => {
@@ -528,7 +529,7 @@ function AdminStudent() {
                       d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
                     />
                   </svg>
-                </button> 
+                </button>
                 {Array.from({ length: numberOfPages }, (_, i) => (
                   <button
                     key={i}
