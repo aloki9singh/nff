45ded100102ecde4d691fd73a65dd288a62b45c4
aloksingh1st <aloksingh1st@gmail.com>
2023-08-06 23:17:41 +0530

    payment Cheking

--- pages/api/payment/index.js
+++ pages/api/payment/index.js
@@ -3,6 +3,12 @@ function encodeToBase64(str) {
   return btoa(str);
 }
 
+const generateTransactionId = () => {
+  const timestamp = Date.now();
+  const randomNum = Math.floor(Math.random() * 1000000); // You can adjust the range as needed
+  return `TXN${timestamp}${randomNum}`;
+};
+
 async function sha256(input) {
   const encoder = new TextEncoder();
   const data = encoder.encode(input);
@@ -12,23 +18,24 @@ async function sha256(input) {
   return hashHex;
 }
 
-async function handler(req, res) {
 
+async function handler(req, res) {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const merchantId = process.env.NEXT_MERCHANT_ID;
   const saltKey = process.env.NEXT_SALT_KEY;
-
+  const payUri = process.env.NEXT_PROD_PAY_URL;
+  const transactionId = generateTransactionId();
 
     const body = JSON.parse(req.body);
     const paymentData =
     {
     "merchantId": merchantId,
-    "merchantTransactionId": "MTST50590068188178",
-    "merchantUserId": "MUID409",
+    "merchantTransactionId": transactionId,
+    "merchantUserId": transactionId+"1",
     "amount": body.price,
     "redirectUrl": baseUrl + `/api/payment/serverToServer?param1=${body.useruid}`,
     "redirectMode": "GET",
-    "callbackUrl": baseUrl +"/api/payment/serverToServer",
+    "callbackUrl": "https://webhook.site/callback-url",
     "mobileNumber": "9999999999",
     "paymentInstrument": {
       "type": "PAY_PAGE"
@@ -58,7 +65,7 @@ async function handler(req, res) {
               };
         
               
-              fetch('https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay', options)
+              fetch("https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay", options)
                 .then(response => response.json())
                 .then(response => {
                     res.status(200).json(response)
--- pages/api/payment/serverToServer.js
+++ pages/api/payment/serverToServer.js
@@ -15,9 +15,8 @@ async function handler(req, res) {
 
   const { param1 } = req.query;
   const body = req.body;
-  console.log("callback");
-
-
+  
+  
   try {
 
 
@@ -25,14 +24,15 @@ async function handler(req, res) {
     const prividerRefId = body.providerReferenceId;
     const transactionId = body.transactionId;
     const result = body.code;
-
+    
     const currentDate = new Date();
     const futureDate = new Date();
     let plan;
-
-
+    
+    
     if (result == "PAYMENT_SUCCESS") {
-
+      
+      console.log("callback");
       if (amount == 29900) {
 
         futureDate.setDate(currentDate.getDate() + 1 * 30);
@@ -94,12 +94,13 @@ async function handler(req, res) {
       res.status(302).redirect(baseUrl + '/beta/payment?val='+payload);
     }
     else {
+      console.log("body", body);
       res.status(302).redirect(baseUrl + '/beta/paymentFailed')
     }
 
   } catch (error) {
     console.log(error);
-    res.status(302).redirect(baseUrl + '/beta/paymentFailed')
+    // res.status(302).redirect(baseUrl + '/beta/paymentFailed')
     // res.status(302).redirect(baseUrl+'/beta/payment');
   }
 }
