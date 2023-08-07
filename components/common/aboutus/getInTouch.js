56d6f4b7ef4b55fe797f302cf65002e4e1d9637b
Tushar123 <jain0735@gmail.com>
2023-08-07 18:09:00 +0530

    errors resolved

--- components/common/aboutus/getInTouch.js
+++ components/common/aboutus/getInTouch.js
@@ -31,6 +31,7 @@ const GetInTouch = () => {
 						<input
 							type="text"
 							id="name"
+							value={name}
 							onChange={(e) => setName(e.target.value)}
 							className="py-2 md:px-3 align-start border-0 border-b border-white text-white dark:text-gray-300 focus:ring-none focus:border-none dark:bg-transparent dark:border-white dark:focus:ring-none dark:focus:border-none  bg-transparent  outline-0 w-[90%] sm:w-[80%] md:w-[28rem] m-auto"
 							placeholder="Your Name"
@@ -40,6 +41,7 @@ const GetInTouch = () => {
 					<div className="flex flex-col">
 						<input
 							type="email"
+							value={email}
 							id="email"
 							onChange={(e) => setEmail(e.target.value)}
 							className="py-2 px-3  border-0 border-b border-white   text-white dark:text-gray-300 focus:ring-none focus:border-none dark:bg-transparent dark:border-white dark:focus:ring-none dark:focus:border-none  bg-transparent outline-none w-[90%] sm:w-[80%] md:w-[28rem] m-auto"
@@ -49,8 +51,9 @@ const GetInTouch = () => {
 					</div>
 					<div className="flex flex-col">
 						<input
-							type="tel"
+							type="number"
 							maxLength={10}
+							value={phoneNo}
 							id="phone"
 							onChange={(e) => setPhoneNo(e.target.value)}
 							className="py-2 px-3  border-0 border-b border-white   text-white dark:text-gray-300 focus:ring-none focus:border-none   dark:focus:ring-none  dark:bg-transparent dark:border-white dark:focus:ring-none dark:focus:border-none  bg-transparent  outline-none w-[90%] sm:w-[80%] md:w-[28rem] m-auto"
@@ -62,6 +65,7 @@ const GetInTouch = () => {
 						<input
 							type="text"
 							onChange={(e) => setTitle(e.target.value)}
+							value={title}
 							id="title"
 							className="py-2 px-3  border-0 border-b border-white   text-white dark:text-gray-300 focus:ring-none focus:border-none   dark:focus:ring-none dark:focus:border-none dark:bg-transparent dark:border-white dark:focus:ring-none   bg-transparent  outline-none w-[90%] sm:w-[80%] md:w-[28rem] m-auto"
 							placeholder="Title/Position"
@@ -71,6 +75,7 @@ const GetInTouch = () => {
 					<div className="flex flex-col">
 						<input
 							type="text"
+							value={subject}
 							onChange={(e) => setSubject(e.target.value)}
 							id="subject"
 							className="py-2 px-3  border-0 border-b border-white  dark:text-gray-300 focus:ring-none focus:border-none da dark:focus:ring-none dark:bg-transparent dark:border-white dark:focus:ring-none dark:focus:border-none bg-transparent text-white outline-0 w-[90%] sm:w-[80%] md:w-[28rem] m-auto"
@@ -82,9 +87,8 @@ const GetInTouch = () => {
 						<button
 							onClick={(e) => {
 								contactFnaboutus(e, formData);
-
 								alert("Message Sent!");
-								clearFormFields();
+								clearFormFields()
 							}}
 							type="submit"
 							className="py-3 px-8 md:px-40 text-sm font-medium text-center bg-[#E1348B] text-white rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-[90%] sm:w-[80%] md:w-[28rem]"
