8f92d5037ab1bd066add73b7a1943bd5acd0aa03
aloksingh1st <aloksingh1st@gmail.com>
2023-08-08 23:23:57 +0530

    debug payment

--- pages/api/payment/serverToServer.js
+++ pages/api/payment/serverToServer.js
@@ -25,7 +25,7 @@ async function handler(req, res) {
     const transactionId = body.transactionId;
     const result = body.code;
 
-    const discountPercentage = process.env.NEXT_PUBLIC_DISCOUNT_PERCENTAGE;
+    const discountPercentage  = process.env.NEXT_PUBLIC_DISCOUNT_PERCENTAGE;
 
     // MRP = Discounted Price / (1 - (Discount Percentage / 100))
 
@@ -36,90 +36,94 @@ async function handler(req, res) {
     let plan;
 
 
-    // if (result == "PAYMENT_SUCCESS") {
+    if (result == "PAYMENT_SUCCESS") {
 
-    console.log("callback");
-    if (price < 500) {
+      console.log("callback");
+      if (price < 500) {
 
-      futureDate.setDate(currentDate.getDate() + 1 * 30);
-      plan = 1;
+        futureDate.setDate(currentDate.getDate() + 1 * 30);
+        plan = 1;
+      }
+
+
+      else if (price >= 500 && price < 1500) {
+        futureDate.setDate(currentDate.getDate() + 6 * 30);
+        plan = 6;
+
+      }
+
+      else{
+        futureDate.setDate(currentDate.getDate() + 12 * 30);
+        plan = 12;
+      }
+
+      const validityData = {
+        trial: {
+          trialStartDate: currentDate,
+          trialEndDate: futureDate,
+        },
+        courseAccess: true,
+      };
+
+
+      // searching if user exists or not
+      const userRef = doc(db, "allusers", param1);
+      const docSnap = await getDoc(userRef);
+      if (docSnap.exists()) {
+        // exist condition update the doc
+        await updateDoc(userRef, validityData);
+
+
+      }
+
+
+      await setDoc(doc(db, "allusers", param1, "PlanInfo", transactionId), {
+        id: param1,
+        amount: amount,
+        providerRefId: prividerRefId,
+        transactionId: transactionId,
+        paymentResult: result,
+        plan: plan,
+        paymentAt: new Date(),
+      });
+
+
+      const payload = encodeToBase64(JSON.stringify({
+        amount: price,
+        providerRefId: prividerRefId,
+        transactionId: transactionId,
+        paymentResult: result,
+        discount:(price - amount),
+        plan: plan,
+        page: 2,
+      }));
+
+      // res.status(302).redirect(baseUrl + '/beta/payment?val='+payload);
+      res.setHeader('Location', baseUrl + '/beta/payment?val=' + payload);
+      res.status(302).end();
     }
-
-
-    else if (price >= 500 && price < 1500) {
-      futureDate.setDate(currentDate.getDate() + 6 * 30);
-      plan = 6;
-
-    }
-
     else {
-      futureDate.setDate(currentDate.getDate() + 12 * 30);
-      plan = 12;
+      console.log("body", body);
+
+      res.status(500).send(body);
+      // const payload = encodeToBase64(JSON.stringify(body));
+      // res.status(302).redirect(baseUrl + '/beta/paymentFailed')
+      // res.setHeader('Location', baseUrl + '/beta/paymentFailed?val=' + payload);
+      // res.status(302).end();
     }
-
-    const validityData = {
-      trial: {
-        trialStartDate: currentDate,
-        trialEndDate: futureDate,
-      },
-      courseAccess: true,
-    };
-
-
-    // searching if user exists or not
-    const userRef = doc(db, "allusers", param1);
-    const docSnap = await getDoc(userRef);
-    if (docSnap.exists()) {
-      // exist condition update the doc
-      await updateDoc(userRef, validityData);
-
-
-    }
-
-
-    await setDoc(doc(db, "allusers", param1, "PlanInfo", transactionId), {
-      id: param1,
-      amount: amount,
-      providerRefId: prividerRefId,
-      transactionId: transactionId,
-      paymentResult: result,
-      plan: plan,
-      paymentAt: new Date(),
-    });
-
-
-    const payload = encodeToBase64(JSON.stringify({
-      amount: price,
-      providerRefId: prividerRefId,
-      transactionId: transactionId,
-      paymentResult: result,
-      discount: (price - amount),
-      plan: plan,
-      page: 2,
-    }));
-
-    // res.status(302).redirect(baseUrl + '/beta/payment?val='+payload);
-    res.setHeader('Location', baseUrl + '/beta/payment?val=' + payload);
-    res.status(302).end();
-
-
-  // }
-    // else {
-    //   console.log("body", body);
-    //   const payload = encodeToBase64(JSON.stringify(body));
-    //   res.setHeader('Location', baseUrl + '/beta/paymentFailed?val=' + payload);
-    //   res.status(302).end();
-    // }
 
   } catch (error) {
-  console.log(error);
+    console.log(error);
 
-  const payload = encodeToBase64(JSON.stringify(error));
-  res.setHeader('Location', baseUrl + '/beta/paymentFailed?val=' + payload);
-  res.status(302).end();
+    // const payload = encodeToBase64(JSON.stringify(error));
+    // res.setHeader('Location', baseUrl + '/beta/paymentFailed?val=' + payload);
+    // res.status(302).end();
 
-  // res.status(302).redirect(baseUrl+'/beta/payment');
-}
+
+    res.status(500).send(error);
+
+    // res.status(302).redirect(baseUrl+'/beta/payment');
+  }
 }
 
 
