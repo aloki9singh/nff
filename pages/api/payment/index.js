52567ba27f59b1ab0295a80efc5c079a21d9615d
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-08-08 20:41:33 +0530

    payment

--- pages/api/payment/index.js
+++ pages/api/payment/index.js
@@ -19,65 +19,55 @@ async function sha256(input) {
   return hashHex;
 }
 
-
 async function handler(req, res) {
-
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const merchantId = process.env.NEXT_MERCHANT_ID;
   const saltKey = process.env.NEXT_SALT_KEY;
   const transactionId = generateTransactionId();
 
-    const body = JSON.parse(req.body);
-    const paymentData =
-    {
-    "merchantId": merchantId,
-    "merchantTransactionId": transactionId,
-    "merchantUserId": transactionId+"1",
-    "amount": body.price,
-    "redirectUrl": baseUrl + `/api/payment/serverToServer?param1=${body.useruid}`,
-    "redirectMode": "GET",
-    "callbackUrl": "",
-    "mobileNumber": "9335929565",
-    "paymentInstrument": {
-      "type": "PAY_PAGE"
-    }
+  const body = JSON.parse(req.body);
+  const paymentData = {
+    merchantId: merchantId,
+    merchantTransactionId: transactionId,
+    merchantUserId: transactionId + "1",
+    amount: body.price,
+    redirectUrl: baseUrl + `/api/payment/serverToServer?param1=${body.useruid}`,
+    redirectMode: "GET",
+    callbackUrl: baseUrl + `/api/payment/serverToServer?param1=${body.useruid}`,
+    mobileNumber: "9335929565",
+    paymentInstrument: {
+      type: "PAY_PAGE",
+    },
+  };
+
+  const encodedData = encodeToBase64(JSON.stringify(paymentData));
+  const shaFormula = encodedData + "/pg/v1/pay" + saltKey;
+  const shaData = await sha256(shaFormula);
+  const shaVerify = shaData + "###1";
+
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "X-VERIFY": shaVerify,
+      },
+      body: JSON.stringify({
+        request: encodedData,
+      }),
+    };
+
+    fetch("https://api.phonepe.com/apis/hermes/pg/v1/pay", options)
+      .then((response) => response.json())
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => console.error(err));
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ msg: "Something went wrong!" + error });
   }
-
-
-    const encodedData = encodeToBase64(JSON.stringify(paymentData));
-    const shaFormula = encodedData+"/pg/v1/pay"+saltKey;
-    const shaData = await sha256(shaFormula);
-    const shaVerify = shaData+"###1";
-
-
-
-    try {
-            const options = {
-                method: 'POST',
-                headers: {
-                  accept: 'application/json',
-                  'Content-Type': 'application/json',
-                  'X-VERIFY': shaVerify,
-                },
-                body: JSON.stringify({
-                  request: encodedData
-
-                })
-              };
-        
-              
-              fetch("https://api.phonepe.com/apis/hermes/pg/v1/pay", options)
-                .then(response => response.json())
-                .then(response => {
-                    res.status(200).json(response)
-                })
-                .catch(err => console.error(err));
-
-    } catch (error) {
-      // console.log(error);
-      res.status(500).json({ msg: "Something went wrong!"+error });
-    }
-  }
-
+}
 
 export default handler;
