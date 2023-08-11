75b6759c82764b4c7cc5245661d2f7ed5f5ac5e8
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-08-12 01:30:20 +0530

    Added

--- components/student/subscription.js
+++ components/student/subscription.js
@@ -1,11 +1,10 @@
-import { useAuthContext } from '@/lib/context/AuthContext';
-import { detailadd } from '@/lib/exportablefunctions';
-import React from 'react';
+import { useAuthContext } from "@/lib/context/AuthContext";
+import { detailadd } from "@/lib/exportablefunctions";
+import React, { useState } from "react";
 
 const SubscriptionData = () => {
-
   const { user, userProfile } = useAuthContext();
-
+  const [text, setText] = useState("Refund");
 
   return (
     <div className="text-white md:-mt-16 overflow-y-scroll">
@@ -17,7 +16,7 @@ const SubscriptionData = () => {
               className="border p-2 rounded-[8px]"
               style={{
                 background:
-                  'linear-gradient(0deg, #2E3036, #2E3036),linear-gradient(0deg, #BFC0C2, #BFC0C2)',
+                  "linear-gradient(0deg, #2E3036, #2E3036),linear-gradient(0deg, #BFC0C2, #BFC0C2)",
               }}
             >
               Change Plan
@@ -38,27 +37,25 @@ const SubscriptionData = () => {
               className="border p-2 rounded-[8px] hover:translate-y-1"
               style={{
                 background:
-                  'linear-gradient(0deg, #2E3036, #2E3036),linear-gradient(0deg, #BFC0C2, #BFC0C2)',
+                  "linear-gradient(0deg, #2E3036, #2E3036),linear-gradient(0deg, #BFC0C2, #BFC0C2)",
               }}
               onClick={() => {
                 console.log("hi");
                 detailadd(userProfile.uid, { refundApplied: true });
-                alert('Applied for refund');
-              }
-              }
+                alert("Applied for refund");
+                setText("Applied");
+              }}
             >
-              Refund
+              {text}
             </button>
           </span>
         </div>
         <div className="text-[#FFFFFFAD]">
           {/* <div>card data</div> */}
-          <div className="my-1">
-            This will cancel your subscription.
-          </div>
+          <div className="my-1">This will cancel your subscription.</div>
         </div>
       </div>
-      
+
       <div className="h-fit p-8 bg-[#373A41] rounded-[36px]">
         <div className="flex justify-between">
           <span className="text-2xl ">Cancel Subscription</span>
