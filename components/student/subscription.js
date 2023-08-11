c52cadf8b9d799e1a13cbe3dee905b74fe122bba
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-08-11 17:24:08 +0530

    refund page

--- components/student/subscription.js
+++ components/student/subscription.js
@@ -1,6 +1,12 @@
+import { useAuthContext } from '@/lib/context/AuthContext';
+import { detailadd } from '@/lib/exportablefunctions';
 import React from 'react';
 
 const SubscriptionData = () => {
+
+  const { user, userProfile } = useAuthContext();
+
+
   return (
     <div className="text-white md:-mt-16 overflow-y-scroll">
       <div className="h-fit my-4 p-6 bg-[#373A41] rounded-[36px]">
@@ -26,26 +32,33 @@ const SubscriptionData = () => {
       </div>
       <div className="h-fit p-8 bg-[#373A41] rounded-[36px] my-4">
         <div className="flex justify-between">
-          <span className="text-2xl ">Payment Method</span>
+          <span className="text-2xl ">Apply for Refund</span>
           <span>
             <button
-              className="border p-2 rounded-[8px]"
+              className="border p-2 rounded-[8px] hover:translate-y-1"
               style={{
                 background:
                   'linear-gradient(0deg, #2E3036, #2E3036),linear-gradient(0deg, #BFC0C2, #BFC0C2)',
               }}
+              onClick={() => {
+                console.log("hi");
+                detailadd(userProfile.uid, { refundApplied: true });
+                alert('Applied for refund');
+              }
+              }
             >
-              Update
+              Refund
             </button>
           </span>
         </div>
         <div className="text-[#FFFFFFAD]">
-          <div>card data</div>
+          {/* <div>card data</div> */}
           <div className="my-1">
-            To update your billing information, click on update.
+            This will cancel your subscription.
           </div>
         </div>
       </div>
+      
       <div className="h-fit p-8 bg-[#373A41] rounded-[36px]">
         <div className="flex justify-between">
           <span className="text-2xl ">Cancel Subscription</span>
